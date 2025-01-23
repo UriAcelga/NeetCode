5ce6986ff7f3b30f5022e2f6befb1192cf79d7c5
UriAcelga <urigomez2002@gmail.com>
2025-01-23 10:02:07 -0300

    two-integer-sum-II resuelto con un solo pase del arreglo. Se usan dos punteros simultáneos y el puntero que se modifica en cada iteración depende del valor de la suma comparado con el target.

--- two-integer-sum-II/solution.js
+++ two-integer-sum-II/solution.js
@@ -5,13 +5,13 @@ class Solution {
      * @return {number[]}
      */
     twoSum(numbers, target) {
-        for (let i = 0; i < numbers.length - 1; i++) {
-            for (let j = i + 1; j < numbers.length; j++) {
-                if(numbers[j] > (target - numbers[i]))
-                    break;
-                else if(numbers[j] == (target - numbers[i]))
-                    return [i+1, j+1];
-            }
+        for (let i = 0, j = numbers.length - 1; i < j;) {
+            if(numbers[i] + numbers[j] == target) {
+                return [i+1, j+1];
+            } else if(numbers[i] + numbers[j] > target)
+                j--;
+            else if(numbers[i] + numbers[j] < target)
+                i++;
         }
         return [0, 0]
     }
